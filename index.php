ff6eeed0cd6bda3f17c5325ca463dfafbd6f3cd2
cristian diaz <diazmolina1@gmail.com>
2018-10-02 17:31:54 -0500

    se agrego las 60 preguntas

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+ $pre1=$_GET["pregunta1"];
+ $pre2=$_GET["pregunta2"];
+ $pre3=$_GET["pregunta3"];
+
+ echo "$pre1 <br>";
+ echo " $pre2 <br>";
+ echo "$pre3 <br>";
+ $suma= $pre1 ;
+
+ echo "la suma es : $suma";
+  
+?>
--- list_questions.php
+++ list_questions.php
@@ -13,11 +13,11 @@ $questions = [
     'ponderation' => 0,
   ],
   [
-    'question' => 'Soy bueno para comprender como la gente se siente',
+    'question' => 'Soy bueno(a) para comprender cómo la gente se siente',
     'ponderation' => 0,
   ],
   [
-    'question' => 'Puedo mantener la calma',
+    'question' => 'Puedo mantener la calma cuando estoy molesto',
     'ponderation' => 0,
   ],
   [
@@ -25,12 +25,227 @@ $questions = [
     'ponderation' => 0,
   ],
   [
-    'question' => 'No me importa lo que le suceda a las personas',
+    'question' => 'Me importa lo que les suceda a las personas',
     'ponderation' => 0,
   ],
   [
-    'question' => 'Me es díficil mantener mi colera',
+    'question' => 'Me es díficil controlar mi cólera',
     'ponderation' => 5,
   ],
+  [
+    'question' => 'Es facil decir a la gente cómo me siento',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me gustan todas las personas que conozco',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me siento seguro(a) de mi mismo(a)',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Sé cómo se sienten las personas',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Sé como mantenerme tranquilo(a)',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Intento usar diferentes formas de responder las preguntas difíciles.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Pienso que las cosas que hago salen bien.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Soy capaz de respetar a los demás.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me molesto demasiado de cualquier cosa.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Es fácil para mí comprender las cosas nuevas.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Puedo hablar fácilmente sobre mis sentimientos.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Pienso bien de todas las personas.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Espero lo mejor.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Tener amigos es importante.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Peleo con la gente.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Puedo comprender preguntas difíciles.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me agrada sonreír.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Intento no herir los sentimientos de las personas.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'No me doy por vencido (a) ante un problema hasta que lo resuelvo.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Tengo mal genio.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Nada me molesta.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Es difícil hablar sobre mis sentimientos más íntimos.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Sé que las cosas saldrán bien.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Puedo dar buenas respuestas a preguntas difíciles.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Puedo fácilmente describir mis sentimientos.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Sé cómo divertirme.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Debo decir siempre la verdad.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me molesto fácilmente.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Me agrada hacer cosas para los demás.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'No me siento muy feliz.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Puedo usar fácilmente diferentes modos de resolver los problemas.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Demoro en molestarme.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me siento bien conmigo mismo (a).',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Hago amigos fácilmente.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Pienso que soy el (la) mejor en todo lo que hago.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Para mí es fácil decirle a las personas cómo me siento.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Cuando respondo preguntas difíciles trato de pensar en muchas soluciones.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me siento mal cuando las personas son heridas en sus sentimientos.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Cuando estoy molesto (a) con alguien, me siento molesto (a) por mucho tiempo.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Me siento feliz con la clase de persona que soy.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Soy bueno (a) resolviendo problemas.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Para mí es difícil esperar mi turno.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Me divierte las cosas que hago.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me agradan mis amigos.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'No tengo días malos.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me es difícil decirle a los demás mis sentimientos.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Me fastidio fácilmente.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Puedo darme cuenta cuando mi amigo se siente triste.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me gusta mi cuerpo.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Aún cuando las cosas sean difíciles, no me doy por vencido.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Cuando me molesto actúo sin pensar.',
+    'ponderation' => 5,
+  ],
+  [
+    'question' => 'Sé cuando la gente está molesta aún cuando no dicen nada.',
+    'ponderation' => 0,
+  ],
+  [
+    'question' => 'Me gusta la forma como me veo.',
+    'ponderation' => 0,
+  ],
+
+
+
 
 ];
