e923afc99f44312491e0b6d2059a7b7d08d15eb7
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2018-10-02 17:30:42 -0500

    Add more progress to questions

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Here goes all questions from test baron.
+ */
+?>
+
+<?php
+if (session_id() == '' || !isset($_SESSION)) {
+  // session isn't started
+  session_start();
+}
+?>
+
+<h1>Bienvenido al TEST BARON</h1>
+
+<p>Test creado por la Universidad Autónoma del Perú en el área de innovación</p>
+<p>el test es la versión corta del BARON (60 preguntas)</p>
+
+<p>Por favor llene sus datos</p>
+<form action="/instructions.php" method="post">
+  Nombres<input name="first_name" type="textfield"/>
+  Apellidos<input name="last_name" type="textfield"/>
+  <input type="submit" value="Siguiente">
+</form>
--- questions.php
+++ questions.php
@@ -13,7 +13,13 @@ ini_set('display_errors', '1');
 require_once 'list_questions.php';
 $questionId = isset($_GET['question_id']) ? $_GET['question_id'] -1 : '';
 // The next question to ask to user.
-$nextQuestionId = $questionId + 1;
+$nextQuestionId = $questionId + 2;
+$basePath = 'questions.php';
+$nextQuestionUrl1 = $basePath . '?question_id=' . $nextQuestionId . '&previous_response=1';
+$nextQuestionUrl2 = $basePath . '?question_id=' . $nextQuestionId . '&previous_response=2';
+$nextQuestionUrl3 = $basePath . '?question_id=' . $nextQuestionId . '&previous_response=3';
+$nextQuestionUrl4 = $basePath . '?question_id=' . $nextQuestionId . '&previous_response=4';
+$nextQuestionUrl5 = $basePath . '?question_id=' . $nextQuestionId . '&previous_response=5';
 // Get the question in array format
 // Array
 //    (
@@ -22,12 +28,19 @@ $nextQuestionId = $questionId + 1;
 //    )
 $question = !empty($questions[$questionId]) ? $questions[$questionId] : NULL;
 
+// Here we store answers from previous question:
+if (!empty($_GET['previous_response'])) {
+
+}
+
 ?>
 <?php if (!empty($question)): ?>
   <h1><?php print $question['question']; ?></h1>
-  <form action="questions.php?question_id=<?php print $nextQuestionId ?>" method="get">
-    <button>0</button>
-  </form>
+  <a href="<?php print $nextQuestionUrl1 ?>">1</a>
+  <a href="<?php print $nextQuestionUrl2 ?>">2</a>
+  <a href="<?php print $nextQuestionUrl3 ?>">3</a>
+  <a href="<?php print $nextQuestionUrl4 ?>">4</a>
+  <a href="<?php print $nextQuestionUrl5 ?>">5</a>
 <?php else: ?>
   <h1>Página incorrecta! regresa atrás</h1>
 <?php endif; ?>
