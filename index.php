4a5a931662613c00d3d45dd9906e0be692e52285
cristian diaz <diazmolina1@gmail.com>
2018-10-04 11:06:21 -0500

    estilos formulario y instrucciones

--- index.php
+++ index.php
@@ -11,14 +11,57 @@ if (!isset($_SESSION)) {
 }
 ?>
 
-<h1>Bienvenido al TEST BARON</h1>
+<!DOCTYPE HTML>
 
-<p>Test creado por la Universidad Autónoma del Perú en el área de innovación</p>
-<p>el test es la versión corta del BARON (60 preguntas)</p>
+<html>
 
-<p>Por favor llene sus datos</p>
-<form action="/instructions.php" method="post">
-  Nombres<input name="first_name" type="textfield"/>
-  Apellidos<input name="last_name" type="textfield"/>
-  <input type="submit" value="Siguiente">
-</form>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="css/main.css" type="text/css" media="all">
+    <title>Test Baron</title>
+</head>
+
+<body>
+<div class="container">
+    <form id="data" action="instructions.php" method="post">
+        <h1>Bienvenido al TEST BARON</h1>
+        <p>Test creado por la Universidad Autónoma del Perú en el área de innovación</p>
+        <p>el test es la versión corta del BARON (60 preguntas)</p>
+        <fieldset>
+            <input placeholder="Nombre" name="first_name" type="text"/>
+        </fieldset>
+        <fieldset>
+            <input placeholder="Apellidos" name="last_name" type="text"/>
+        </fieldset>
+        <fieldset>
+            <input placeholder="Sexo" name="sex" type="text"/>
+        </fieldset>
+        <fieldset>
+            <input  placeholder="Edad" name="age" type="text"/>
+        </fieldset>
+        <fieldset>
+            <input placeholder="Grado" name="grade" type="text"/>
+        </fieldset>
+        <fieldset>
+            <input  placeholder="Colegio" name="age" type="text"/>
+        </fieldset>
+        <fieldset>
+            <input  placeholder="Fecha" name="date" type="text"/>
+        </fieldset>
+        <fieldset>
+            <button  type="submit" value="Siguiente">Submit</button>
+        </fieldset>
+    </form>
+
+
+</div>
+
+<footer>
+    <p>Copyright 2018 UA</p>
+</footer>
+
+</body>
+
+</html>
+
+
--- instructions.php
+++ instructions.php
@@ -6,23 +6,61 @@
 
 <?php
 if (!empty($_POST['first_name'])) {
-  $first_name = $_POST['first_name'];
-  if (!isset($_SESSION)) {
-    // session isn't started
-    session_start();
-    $_SESSION['personal_data']['first_name'] = $first_name;
-  }
-  else {
-    // @TODO: Maybe redirect to homepage if no session started?.
-  }
+    $first_name = $_POST['first_name'];
+    if (!isset($_SESSION)) {
+        // session isn't started
+        session_start();
+        $_SESSION['personal_data']['first_name'] = $first_name;
+    } else {
+        // @TODO: Maybe redirect to homepage if no session started?.
+    }
 }
 ?>
 
-<?php if (!empty($_SESSION['personal_data'])): ?>
-  <h1>INDICACIONES DEL TEST BARON</h1>
-  <h2>Estimado <?php print $first_name; ?></h2>
 
-  <p>Tenga en cuenta lo siguiente...</p>
+<!DOCTYPE HTML>
 
-  <a href="questions.php?question_id=1">Siguiente</a>
-<?php endif; ?>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" href="css/instructions.css" type="text/css" media="all">
+    <title>Test Baron </title>
+</head>
+
+<body>
+<div class="container">
+    <?php if (!empty($_SESSION['personal_data'])): ?>
+        <h1>INDICACIONES DEL TEST BARON</h1>
+        <h2>Estimado <?php print $first_name; ?></h2>
+        <p>Lee cada oración y elije una respuesta que mejor te describe, hay cuatro posibles respuestas:</p>
+        <ul>
+            <li>1. Muy rara vez</li>
+            <li>2. Rara vez</li>
+            <li>3. A menudo</li>
+            <li>4. Muy a menudo</li>
+        </ul>
+        <p>
+            Dinos cómo te sientes, piensas o actúas <strong>LA MAYOR PARTE DEL TIEMPO EN LA MAYORIA DE LUGARES.</strong>
+            Elige una, y Sólo <strong>UNA</strong> respuesta para cada oración.Esto no es un examen; no existen respuestas buenas o malas.
+            Por favor selecciona la respuesta en cada oración
+        </p>
+        <a href="questions.php?question_id=1">Siguiente</a>
+    <?php endif; ?>
+</div>
+
+<footer>
+    <p>Copyright 2018 UA</p>
+</footer>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
