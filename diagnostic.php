ff07cad6fdbc360f4225fe73a86f2fa72c9fd68c
cristian diaz <diazmolina1@gmail.com>
2018-10-16 17:53:40 -0500

    funciones  agregadas v2

--- diagnostic.php
+++ diagnostic.php
@@ -23,7 +23,7 @@ print_r(getDirectScore($questions, $_SESSION['questions']));
 function getDirectScore($questionsFromList, $answers) {
   $intrapersonal = getEmotionScore($questionsFromList, $answers, [2, 6, 16, 27, 42, 52]);
   $interpersonal = getEmotionScore($questionsFromList, $answers, [1, 4, 9, 13, 19, 23, 35, 40, 44, 50, 54, 58]);
-  $estres = getEmotionScore($questionsFromList, $answers, [2, 5, 10, 14, 20, 25, 34, 38, 45, 53, 57, 58]);
+  $estres = getEmotionScore($questionsFromList, $answers, [2, 5, 10, 14, 20, 25, 34, 38, 45, 48,53, 57]);
   $adaptabilidad = getEmotionScore($questionsFromList, $answers, [11, 15, 21, 24, 29, 33, 37, 43, 47, 56]);
   $animo = getEmotionScore($questionsFromList, $answers, [0, 3, 8, 12, 18, 22, 28, 31, 36, 39, 46, 49, 55, 59]);
   $impresion = getEmotionScore($questionsFromList, $answers, [7, 17, 26, 32, 41, 51]);
